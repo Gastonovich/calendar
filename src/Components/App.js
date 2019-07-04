cf3016769c508e76c5f2cd969bb49658f8c3abe3
Gastonovich <oleksndrkorn@gmail.com>
2019-07-04 23:33:48 +0300

    all functionality, tip - add css, localstorage

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,24 +7,40 @@ function App() {
   const [date, setDate] = useState(moment().toISOString())
   const [isOpenModal, setIsOpenModal] = useState(false)
   const [events, setEvents] = useState([])
-  const [currentDay, setCurrentDay] = useState(null)
+  const [currentDayObj, setCurrentDayObj] = useState(null)
 
   const renderCalendar = () => {
     let arr = [];
     for (let k = 1; k <= moment(date).daysInMonth(); k++) {
-      arr.push({
-        id: k - 1,
-        number: k
-      })
+      let obj = findEvent(k, moment(date).month(), moment(date).year())
+      if (obj) {
+        arr.push({
+          id: k - 1,
+          number: k,
+          event: obj.event
+        })
+      } else {
+        arr.push({
+          id: k - 1,
+          number: k
+        })
+      }
+
     }
     arr = <ul>
-      {arr.map((el) => <li onClick={() => openModal(el.number)} key={el.id}>{el.number}</li>)}
+      {arr.map((el) => <li onClick={() => openModal({ id: el.number, event: el.event })} key={el.id}>{el.number} {el.event && "*"}</li>)}
     </ul>
     return arr;
   }
 
-  const openModal = (id) => {
-    setCurrentDay(id)
+  const findEvent = (day, month, year) => {
+    console.log(day + " " + month + ' ' + year)
+    return events.find(el => el.day === day && el.month === month && el.year === year)
+
+  }
+
+  const openModal = (obj) => {
+    setCurrentDayObj(obj)
     changeModalState(true)
   }
 
@@ -37,12 +53,14 @@ function App() {
     setDate(newDateObj)
   }
 
-  const addEvent = (event) => {
-    console.log(events.push(event))
-    let arr = events.push(event)
-    setEvents(arr)
-    localStorage.setItem('Events', JSON.stringify(events))
-   
+  const addEvent = (obj) => {
+    let { id, event } = obj;
+    setEvents([...events, {
+      day: id,
+      month: moment(date).month(),
+      year: moment(date).year(),
+      event: event,
+    }])
   }
 
   useEffect(() => {
@@ -55,7 +73,7 @@ function App() {
       <button onClick={() => changeCurrentDate(-1)}>Prev</button>
       <button onClick={() => changeCurrentDate(1)}>Next</button>
       {renderCalendar()}
-      {isOpenModal && <Modal close={changeModalState} day={currentDay} addEvent={addEvent} />}
+      {isOpenModal && <Modal changeModalState={changeModalState} dayObj={currentDayObj} addEvent={addEvent} />}
     </>
   );
 }
--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function modal({ close, day, addEvent }) {
+function Modal({ changeModalState, dayObj, addEvent }) {
 
+  const [inputValue, setInputValue] = useState(null)
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const confirm = () => {
+    if (inputValue.length > 0) {
+      addEvent({ id: dayObj.id, event: inputValue })
+      changeModalState(false)
+    }
+  }
 
   return (
     <>
-      <h1>{day}</h1>
-      <button onClick={()=>addEvent({id: day})}>Add</button>
-      <button onClick={() => close(false)} >Close</button>
+      <h1>{dayObj.id}</h1>
+      <input value={dayObj.event && dayObj.event} onChange={handleChange}></input>
+      <button onClick={confirm}>{dayObj.event === undefined ? 'Add' : 'Change'}</button>
+      <button onClick={() => changeModalState(false)} >Close</button>
     </>
   );
 }
 
-export default modal;
+export default Modal;
